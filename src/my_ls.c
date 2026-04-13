5017a2eb3bd54cbe5e07e3ec1ed1b71167e6d225
zhr <3328785663@qq.com>
2026-04-13 11:49:38 +0800

    实现ls和rm功能

--- src/my_ls.c
+++ src/my_ls.c
@@ -1,5 +1,49 @@
 #include "fs_def.h"
 #include "my_ls.h"
-void my_ls(){
-    
+
+static void trim_copy_name(const char src[8], char out[9]) {
+    memcpy(out, src, 8);
+    out[8] = '\0';
+    int len = 8;
+    while (len > 0 && out[len - 1] == ' ')
+        len--;
+    out[len] = '\0';
+}
+
+static void trim_copy_ext(const char src[3], char out[4]) {
+    memcpy(out, src, 3);
+    out[3] = '\0';
+    int len = 3;
+    while (len > 0 && out[len - 1] == ' ')
+        len--;
+    out[len] = '\0';
+}
+
+void my_ls(void) {
+    uint8_t *curr = GET_BLOCK_ADDR(current_dir_block);
+
+    for (int i = 0; i < DIR_ENTRY_MAX; i++) {
+        DirEntry *e = (DirEntry *)(curr + i * sizeof(DirEntry));
+        if (e->filename[0] == '\0')
+            continue;
+
+        char name[9], ext[4];
+        trim_copy_name(e->filename, name);
+        trim_copy_ext(e->ext, ext);
+
+        if (ext[0] != '\0')
+            printf("%s.%s", name, ext);
+        else
+            printf("%s", name);
+
+        if (e->attribute == FILE_ATTR_DIR)
+            printf("  [目录]");
+        else if (e->attribute == FILE_ATTR_HIDE)
+            printf("  [隐藏]");
+        else
+            printf("  %u 字节", (unsigned)e->file_size);
+
+        printf("\n");
+    }
+    fflush(stdout);
 }
--- src/my_rm.c
+++ src/my_rm.c
@@ -1,5 +1,57 @@
 #include "fs_def.h"
 #include "my_rm.h"
-void my_rm(char* arg){
-    
+
+void my_rm(char *arg) {
+    if (arg == NULL || arg[0] == '\0') {
+        printf("用法: rm <文件名>\n");
+        return;
+    }
+    if (strlen(arg) > 8) {
+        printf("错误: 文件名不能超过8个字符\n");
+        return;
+    }
+
+    uint8_t *curr = GET_BLOCK_ADDR(current_dir_block);
+
+    for (int i = 0; i < DIR_ENTRY_MAX; i++) {
+        DirEntry *entry = (DirEntry *)(curr + i * sizeof(DirEntry));
+        if (entry->filename[0] == '\0')
+            continue;
+        if (strncmp(entry->filename, arg, 8) != 0)
+            continue;
+
+        if (entry->attribute == FILE_ATTR_DIR) {
+            printf("错误: '%s' 是目录，请使用 rmdir 删除\n", arg);
+            return;
+        }
+
+        uint16_t blk = entry->first_block;
+        int steps = 0;
+        while (blk != 0 && blk < super_block->total_blocks && steps < (int)super_block->total_blocks) {
+            steps++;
+            if (blk < super_block->data_start) {
+                printf("错误: 文件数据块号异常\n");
+                return;
+            }
+            uint16_t next = fat_table[blk];
+            if (fat_table[blk] == FAT_FREE)
+                break;
+            fat_table[blk] = FAT_FREE;
+            super_block->free_blocks++;
+            if (next == FAT_END)
+                break;
+            blk = next;
+        }
+
+        for (int j = 0; j < OPEN_FILE_MAX; j++) {
+            if (open_file_table[j].dir_entry == entry)
+                memset(&open_file_table[j], 0, sizeof(OpenFileItem));
+        }
+
+        memset(entry, 0, sizeof(DirEntry));
+        printf("成功删除文件: %s\n", arg);
+        return;
+    }
+
+    printf("错误: 找不到文件 '%s'\n", arg);
 }
